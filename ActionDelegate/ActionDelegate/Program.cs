e2af66773c3533e233f672c63cdf841237e003c5
aashi2308 <42564823+aashi2308@users.noreply.github.com>
2018-09-21 11:31:09 +0530

    Add files via upload

---
+++ ActionDelegate/ActionDelegate/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ActionDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int> action1 = ActionDelegate.GetDetails;
+            action1(1);
+            Action<int> action2 = delegate (int id)
+            {
+                Console.WriteLine(id);
+            };
+            action2(2);
+            Action<int> action3 = id => Console.WriteLine(id);
+            action3(3);
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class ActionDelegate
+    {
+        static List<Student> StudentDetails = new List<Student>()
+        {
+            new Student(){ StudentId=1, StudentName="Ashi"},
+            new Student(){ StudentId=2, StudentName=" Siddhi"}
+        };
+
+        public static void GetDetails(int id)
+        {
+            
+            if (StudentDetails.Select(x => x.StudentId).FirstOrDefault().Equals(id))
+            {
+                Console.WriteLine("The id of the Student is {0}",id);
+            }
+        }
+
+       
+
+    }
+
+    class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+
+        
+    }
+}
